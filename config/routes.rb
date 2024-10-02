233a25023ddc1b8abec9040b9ab393a17ac319ff
yura <yduara2248@gmail.com>
2024-10-03 00:36:39 +0900

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :todos, only: [:index]
     end
-    resources :quizs, param: :quiz_word, only: [:show]
-    resources :rankings, only: [:show, :create]
+    resources :quizs, param: :quiz_num, only: [:show]
+    resources :rankings, only: [:show, :create] do
+      collection do
+        post ':user_name/:score', action: :create
+      end
     end
   end
 end
