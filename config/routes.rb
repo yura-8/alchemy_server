1d7cf533e81bd21eb73c2b40ba2b70713ea6f105
yura <yduara2248@gmail.com>
2024-10-03 00:21:16 +0900

    create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  mount Rswag::Ui::Engine => 'api-docs'
+  mount Rswag::Api::Engine => 'api-docs'
+  
+  namespace :api , format: 'json'do
+    namespace :v1 do
+      resources :todos, only: [:index]
+    end
+    resources :quizs, param: :quiz_word, only: [:show]
+    resources :rankings, only: [:show, :create]
+    end
+  end
 end
