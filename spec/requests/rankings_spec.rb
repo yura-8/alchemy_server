c01f2cd8b3463cc16fc00dc32e26c1c5076d895f
yura <yduara2248@gmail.com>
2024-10-05 17:37:19 +0900

    fix test

--- spec/requests/rankings_spec.rb
+++ spec/requests/rankings_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Rankings", type: :request do
   describe "GET /index" do
-    
     it "return a 200 response" do
       get "/rankings"
       expect(response).to have_http_status "200"
