3858d4099021361c4d88236f0c99ab5e858cff0e
Ishigami100 <yamahaya5151@icloud.com>
2024-10-05 17:35:08 +0900

    formatを直す

--- spec/requests/quizs_spec.rb
+++ spec/requests/quizs_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Quizs", type: :request do
   describe "GET /quizs" do
-    context " num is 1"do
+    context "num is 1" do
       it "returns http success" do
         get "/quizs/1"
         expect(response).to have_http_status(:success)
         json_response = JSON.parse(response.body)
-        expect(json_response["quiz"].length).to eq(1) 
+        expect(json_response["quiz"].length).to eq(1)
       end
     end
-    context " num is 5"do
+
+    context "num is 5" do
       it "returns http success" do
         get "/quizs/5"
         expect(response).to have_http_status(:success)
         json_response = JSON.parse(response.body)
-        expect(json_response["quiz"].length).to eq(5) 
+        expect(json_response["quiz"].length).to eq(5)
       end
     end
   end
