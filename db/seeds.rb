685a838ad25a08fde0a570aee6abdea9ffa29832
Ishigami100 <yamahaya5151@icloud.com>
2024-10-05 12:36:27 +0900

    初期の値を新規追加する

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,19 @@
 require 'csv'
 
 CSV.foreach('db/ejdict.csv',quote_char: "\x00", force_quotes: true) do |row|
-    p  row[0]
     Quiz.create(:quiz_word => row[0], :quiz_meaning => row[1])
 end
+
+CSV.foreach('db/lowquiz.csv') do |row|
+    Lowquiz.create(:quiz_word => row[0], :quiz_meaning => row[1])
+end
+
+Ranking.create!(
+    user_name: "Hanako",
+    score: 10,
+)
+
+Ranking.create!(
+    user_name: "Kenzi",
+    score: 20,
+)
