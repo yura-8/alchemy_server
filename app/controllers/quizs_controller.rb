fdcbef0fbb5e6b9e1eb4103adda1f47041d7b3cb
yura <yduara2248@gmail.com>
2024-10-05 11:54:59 +0900

    clear api

--- app/controllers/quizs_controller.rb
+++ app/controllers/quizs_controller.rb
@@ -1,14 +1,14 @@
 class QuizsController < ApplicationController
     def show
-        #p quiz_num = params[:id].to_i
-        #random_quizs = Quiz.limit(3)
-        #render json: {
-        #    quiz: random_quizs.map do |quiz|
-        #    {
-        #        word: quiz.quiz_word,
-        #        meaning: quiz.quiz_meaning
-        #    }
-        #    end
-        #}
+        p quiz_num = params[:id].to_i
+        random_quizs = Quiz.limit(quiz_num)
+        render json: {
+            quiz: random_quizs.map do |quiz|
+            {
+                word: quiz.quiz_word,
+                meaning: quiz.quiz_meaning
+            }
+            end
+        }
     end
 end
